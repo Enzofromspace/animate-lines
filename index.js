525f2922f1ce2e566d33b8ee45f4fdeb2c250e31
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-15 15:10:17 +0300

    created function in vector class to calculate distance

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ class Vector {
     this.x = x;
     this.y = y;
   }
+  getDistance(v){
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
 }
 
 class Agent {
